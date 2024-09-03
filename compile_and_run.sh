41093613fb3133eed6aed041c28afb17643485a9
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-03 20:58:45 +0200

    Cleaned up compile_and_run.sh

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-
-# Navigate to the directory where the script is located
 cd "$(dirname "$0")"
 
-# Remove the build directory if it exists
 rm -rf build
 
-# Create a new build directory
 mkdir build
 
-# Navigate into the build directory
 cd build
 
-# Run CMake to configure the project
 cmake ..
 
-# Build the project
 cmake --build .
 
-clear
-
-# Run the resulting executable
+echo ""
 echo "####################"
 echo "# Running './main' #"
 echo "####################"
