e84fa8a798e4572bb6b8f6d0c4b4b1369a538c5a
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-04 19:39:45 +0200

    Split compile_and_run into make and run and clean_and_recompile so I don't have to invoke cmake .. every time (slows down compilation speed)

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-rm -rf build
-
-mkdir build
-
 cd build
-
-cmake ..
 
 cmake --build .
 
-echo ""
-echo "####################"
-echo "# Running './main' #"
-echo "####################"
-echo ""
 ./main
